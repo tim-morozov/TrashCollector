d992cba844e9e897f977bb5bfdf8a7723c6bfce1
tim-morozov <tim.morozov@gmail.com>
2020-05-20 16:12:51 -0500

    added variable in GlobalRouting and constructor

--- Trash_Collector/Trash_Collector/ActionFilters/GlobalRouting.cs
+++ Trash_Collector/Trash_Collector/ActionFilters/GlobalRouting.cs
@@ -2,12 +2,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Trash_Collector.ActionFilters
 {
     public class GlobalRouting : IActionFilter
     {
+        private readonly ClaimsPrincipal _claimsPrincipal;
+        public GlobalRouting(ClaimsPrincipal claimsPrincipal)
+        {
+            _claimsPrincipal = claimsPrincipal;
+        }
         public void OnActionExecuted(ActionExecutedContext context)
         {
             throw new NotImplementedException();
