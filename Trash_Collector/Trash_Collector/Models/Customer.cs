1311f3b315946febeb921ab02384c719c18f93b5
tim-morozov <tim.morozov@gmail.com>
2020-05-20 15:49:46 -0500

    added customer model

---
+++ Trash_Collector/Trash_Collector/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trash_Collector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set;}
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
